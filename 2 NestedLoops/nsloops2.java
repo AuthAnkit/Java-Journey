019b9afecab4c6e7631cfbdad35b4f051b1a1443
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-09-07 18:49:15 +0530

    Add files via upload

---
+++ 2 NestedLoops/nsloops2.java
@@ -0,0 +1,78 @@
+/*
+ * Rectangle Pattern
+You are given two integers N and M.
+
+You are required to write a program that prints a Rectangle of size N * M.
+
+
+Pattern for N = 3 and M = 5
+
+* * * * * 
+* * * * * 
+* * * * * 
+
+Input Format
+single line consisting of two integers N and M.
+
+Output Format
+Print the required pattern
+
+Example 1
+Input
+
+
+3 5
+
+Output
+
+
+* * * * * 
+* * * * * 
+* * * * * 
+
+Explanation
+
+
+Pattern for N = 3 and M = 5
+
+Example 2
+Input
+
+
+4 2
+
+Output
+
+
+* * 
+* * 
+* * 
+* * 
+ 
+
+Explanation
+
+
+Pattern for N = 4 and M = 2
+
+Constraints
+1 <= N , M <= 100
+Topics
+ */
+
+import java.util.*;
+public class nsloops2 {
+public static void main(String[] args) {
+    try(Scanner sc  = new Scanner(System.in)){
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i=1;i<=n;i++){
+        for (int j = 1; j <= m; j++) {
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+    }
+    }
+}
+    
+}
