9735f955e18cd1cdba53ce5b0855be4ca677b79e
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-09-11 18:37:31 +0530

    Add files via upload

---
+++ 2 NestedLoops/nsloops11.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+/*Print Number Pattern 2
+Given an integer n, print a pattern of n lines, where the ith line has the numbers i to 1 printed in descending order. Check the sample cases for a better understanding.
+
+Input Format:
+Input consists of an integer N.
+
+Output Format:
+Print the pattern as described above.
+
+Example 1
+Input:
+
+5
+Output:
+1
+21
+321
+4321
+54321
+
+Pattern Image
+
+Example 2
+Input:
+
+3
+Output::
+1
+21
+321
+
+Pattern Image
+
+Constraints
+1 <= n <= 9
+
+Topics
+Loops
+*/public class nsloops11 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            int n = sc.nextInt();
+
+            for(int i=1;i<=n;i++){
+                for(int j=i;j>=1;j--){
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
