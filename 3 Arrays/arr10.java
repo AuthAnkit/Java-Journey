7d9c93a86ad4dd1fa331564b0a4375face4c4257
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-09-14 13:02:43 +0530

    Add files via upload

---
+++ 3 Arrays/arr10.java
@@ -0,0 +1,64 @@
+/*Array Subtracting
+You have given a number n1, representing the size of array arr1. You have given n1 numbers, representing elements of array arr1. You have given a number n2, representing the size of array arr2.You have given n2 numbers, representing elements of array arr2. The two arrays represent digits of two numbers.
+
+You are required to subtract the numbers represented by two arrays and print the arrays. n1 and n2 can be of different sizes
+
+Input Format
+First line consists of an integer n1
+
+Second line consists of n1 spaced integers, representing elements of arr1
+
+Third line consists of an integer n2
+
+Fourth line consists of n2 spaced integers, representing elements of arr2
+
+Output Format
+Subtract the two numbers and print the array
+
+Example 1
+Input
+
+3
+3 2 1
+3
+1 2 3
+Output
+
+1
+9
+8
+Explanation
+
+321 - 123 = 198
+
+Example 2
+Input
+
+4
+1 2 3 4
+2
+2 1
+Output
+
+1
+2
+1
+3
+Explanation
+
+1234 - 21 = 1213
+
+Constraints
+1 <= n1, n2 <= 10
+
+0 <= a1[i], a2[i] < 10
+
+Topics
+Number System: Number Problems
+Arrays
+Array - Basics
+*/
+
+
+
+//later 
