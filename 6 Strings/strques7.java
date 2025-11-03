8c660a91321dfd9292f1ab2bb102324f574df196
Ankit singh <167414480+AuthAnkit@users.noreply.github.com>
2025-11-03 19:40:33 +0530

    Add files via upload

---
+++ 6 Strings/strques7.java
@@ -0,0 +1,126 @@
+/*
+ *Compressed Strings
+You are given a string 'S'. You need to compress it using the following algorithms:
+
+Take an empty string res. For every block of consecutive repeating character, do the following:
+
+If the size of block is 1, concatenate it to the end of `S`.
+Else, concatenate the character followed by the size of the block.
+
+Return res after completing the above operations on the whole string
+
+Note
+
+StringBuilder in Java represents a mutable sequence of characters.
+
+Input Format
+The first line contains the number of test cases.
+
+For each test case: The first line contains the string 'S'
+
+Output Format
+For each test case return the string res. Input and output is inbuilt.
+
+Example 1
+Input:
+
+1
+abbbccd
+Output:
+
+ab3c2d
+Explanation:
+
+a occurs once, so, we add a to res.
+
+Then, b is repeated 3 times so, b3 is added.
+
+Then, c is repeated 2 times so, c2 is added.
+
+d occurs once, so, we add d to res.
+
+Example 2
+Input:
+
+1
+aabccc
+Output:
+
+a2bc3
+Explanation:
+
+a is repeated 2 times so, a2 is added.
+
+b occurs once, so, we add b to res.
+
+Then, c is repeated 3 times so, c3 is added.
+
+Constraints
+1 <= T <= 10
+1 <= |S| <= 100000
+Topics
+Strings - Basics
+Strings
+Companies
+Amazon
+Microsoft 
+ */
+
+
+import java.util.*;
+public class strques7{
+    public static void CompressedString(String str){
+        int n = str.length();
+
+
+        //WAY ONE (1)
+       /*for(int i=0;i<n;i++){
+        int count =1;
+        char ch = str.charAt(i);
+            for(int j=i;j<n-1;j++){
+                if(str.charAt(j+1)==ch){
+                    count ++;
+                    i++;
+                }
+                else if(str.charAt(j+1)!=ch){
+                    break;
+                }
+            }
+            System.out.print(ch);
+            if(count>1){
+                System.out.print(count);
+            }
+       }
+            */
+
+
+//Way 2
+
+        for(int i=0;i<n;i++){
+            char ch = str.charAt(i);
+            int count =1 ;
+
+            while (i<n-1 && str.charAt(i+1)==ch) {
+                    count++;
+                    i++;
+            }
+
+            System.out.print(ch);
+            if(count>1){
+                System.out.print(count);
+            }
+        }
+}
+public static void main(String[] args) {
+    try(Scanner sc = new Scanner(System.in)){
+        String str = sc.nextLine();
+        CompressedString(str);
+
+        for(int i=0;i<10;i=i+2){
+            System.out.println(i);
+            
+        }
+    }
+}}
+
+
